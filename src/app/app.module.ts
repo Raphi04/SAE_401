e3e440623c7233d8b805474309a834ecd1e3c47f
Raphi04 <raphaelcadete04@gmail.com>
2024-03-10 17:59:26 +0100

    Inscription faite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +31,8 @@ import { BoxComponent } from './component/box/box.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/inscription/inscription.component.ts
+++ src/app/component/inscription/inscription.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { NavService } from '../../services/nav.service';
 import { HeaderService } from '../../services/header.service';
+import { FormBuilder, FormControl, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
@@ -9,8 +10,153 @@ import { HeaderService } from '../../services/header.service';
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent implements OnInit, OnDestroy{
-  constructor(private headerService : HeaderService, private navService : NavService) {}
+  constructor( private headerService : HeaderService, private navService : NavService, private form: FormBuilder, private router : Router ) {}
 
+  //Création du formulaire
+  inscriptForm = this.form.group({
+    email: ["", [Validators.required, Validators.email]],
+    motDePasse : ["", [Validators.required, Validators.minLength(8)]],
+    cMotDePasse : ["", [Validators.required, Validators.minLength(8)]]
+  })
+
+  //Flag pour la première fois que le formulaire apparait
+  isSubmited = false;
+  emailErrorMessage = "";
+  motDePasseErrorMessage = "";
+  cMotDePasseErrorMessage = "";
+  errorForm = true;
+
+  onSubmit() {
+    this.isSubmited = true;
+    this.checkError("motDePasse");
+    this.checkError("cMotDePasse");
+    this.checkError("email");
+
+      if(!this.errorForm) {
+
+        //Récupération des utilisateurs
+        if(localStorage.getItem("allUsers") !== null) {
+          let allUsers = JSON.parse(localStorage.getItem("allUsers") || "[]");
+
+          //Création du nouvel utilisateur
+          let newUser = {
+            email : this.inscriptForm.get("email")?.value,
+            mdp : this.inscriptForm.get("motDePasse")?.value
+          };
+
+          //Ajout du nouvel utilisateur
+          allUsers.push(newUser);
+          localStorage.setItem("allUsers", JSON.stringify(allUsers));
+
+          //Redirection
+          this.router.navigate(["/app-connexion"]);
+
+        } else {
+          
+          //Création du tableau d'utilisateurs
+          let allUsers = [];
+
+          //Création du nouvel utilisateur
+          let newUser = {
+            email : this.inscriptForm.get("email")?.value,
+            mdp : this.inscriptForm.get("motDePasse")?.value
+          };
+
+          //Ajout de l'utilisateur
+          allUsers.push(newUser);
+          localStorage.setItem("allUsers", JSON.stringify(allUsers));
+
+          //Redirection
+          this.router.navigate(["/app-connexion"]);
+        }
+    }
+  }
+
+  checkError(input: string) {
+    const myInput = this.inscriptForm.get(input);
+    switch (input) {
+      case "email" :
+        this.errorForm = false;
+        if(myInput?.invalid && (myInput?.dirty || myInput?.touched || this.isSubmited)) {
+          if(myInput?.errors?.["email"]) {
+            this.emailErrorMessage = "Veuillez saisir un email correct";
+          } else {
+            this.emailErrorMessage = "Veuillez remplir ce champ";
+          }
+          this.errorForm = true;
+          return "errorInput";
+        } else {
+
+          //Test pour vérifier si l'email existe déjà
+          if(localStorage.getItem("allUsers") !== null && (myInput?.dirty || myInput?.touched || this.isSubmited)) {
+            let allUser = JSON.parse(localStorage.getItem("allUsers") || "[]");
+            allUser.forEach((user : any) => {
+              if(user["email"] == myInput?.value) {
+                this.emailErrorMessage = "Email déjà attribué";
+                this.errorForm = true;
+              }
+            });
+            if (this.errorForm) {
+              return "errorInput";
+            }
+            this.emailErrorMessage = "";
+            this.errorForm = false;
+            return "noError";
+          }
+          this.emailErrorMessage = "";
+          if(this.isSubmited) {
+            this.errorForm = false;
+          }
+          return "noError";
+        }      
+
+
+      case "motDePasse" :
+        if(myInput?.invalid && (myInput?.dirty || myInput?.touched ||this.isSubmited)) {
+          if(myInput?.errors?.['minlength']) {
+            this.motDePasseErrorMessage = "Au moins 8 caractères sont attendus";
+          } else {
+            this.motDePasseErrorMessage = "Veuillez remplir ce champ";
+          }
+          this.errorForm = true;
+          return "errorInput";
+        } else {
+          this.motDePasseErrorMessage = "";
+          if(this.isSubmited) {
+            this.errorForm = false;
+          }
+          return "noError";
+        }
+      
+
+
+      case "cMotDePasse" :
+        if(myInput?.invalid && (myInput?.dirty || myInput?.touched ||this.isSubmited)) {
+          if(myInput?.errors?.['minlength']){
+            this.cMotDePasseErrorMessage = "Au moins 8 caractères sont attendus";
+          } else {
+            this.cMotDePasseErrorMessage = "Veuillez remplir ce champ";
+          }
+          this.errorForm = true;
+          return "errorInput";
+        } else {
+            if (myInput?.value !== this.inscriptForm.get("motDePasse")?.value && this.inscriptForm.get("motDePasse")?.touched) {
+              this.cMotDePasseErrorMessage = "Veuillez écrire le même mot de passe";
+              this.errorForm = true;
+              return "errorInput";
+            } else {
+              this.cMotDePasseErrorMessage = "";
+              if(this.isSubmited) {
+                this.errorForm = false;
+              } 
+              return "noError";
+            }
+          } 
+        }
+    return "noError";
+    }
+      
+  //Gestion de l'affichage du header et de la nav
   ngOnInit(): void {
     this.headerService.hide();
     this.navService.hide();
